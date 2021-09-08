615198ec43c71ebc0a7cfa1de3dbb2cb801924c9
divyeshbhalekar <89533742+divyeshbhalekar@users.noreply.github.com>
2021-09-08 17:28:58 +0530

    added a change to app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react, i was changed.
         </a>
       </header>
     </div>
